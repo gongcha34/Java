a47ccadc2114ae635b034fd4cc531b7d7327a16f
barn485 <barn485@gmail.com>
2020-11-04 12:38:39 +0900

    escape sequence

--- Tutorial2/src/Main.java
+++ Tutorial2/src/Main.java
@@ -2,9 +2,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		String s = "Test!";
 		System.out.println(s);
+		
+		//이스케이스 시퀀스(escape sequence)
+		String es = "Test1\"Test2\"";
+		System.out.println(es);
+		
 	}
 
 }
