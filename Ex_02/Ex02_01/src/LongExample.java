afb02681a1a4266b1fc9240bb6bdfa172b92d9ca
Chacha <66931142+gongcha34@users.noreply.github.com>
2020-11-04 15:39:30 +0900

    Ex_02

---
+++ Ex_02/Ex02_01/src/LongExample.java
@@ -0,0 +1,17 @@
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		// long 타입 변수
+		long var1 = 10;
+		long var2 = 20L;
+		//long var3 = 100000000000000; //컴파일에러
+		long var4 = 100000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+
+	}
+
+}
---
+++ Ex_02/Ex02_01/src/VariableExample.java
@@ -0,0 +1,14 @@
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+
+		//변수 선언 초기화!
+		int value = 10;
+		int result = value + 10;
+		
+		System.out.println(result);
+
+	}
+
+}
