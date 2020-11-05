7f23166917c86f1e5999f2dd6c336caf367797f8
barn485 <barn485@gmail.com>
2020-11-05 12:21:33 +0900

    First commit

--- Tutorial2/src/Main.java
+++ Tutorial2/src/Main.java
@@ -3,12 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String s = "Test!";
+		String s = "Hello World!";
 		System.out.println(s);
-		
-		//이스케이스 시퀀스(escape sequence)
-		String es = "Test1\"Test2\"";
-		System.out.println(es);
 		
 	}
 
