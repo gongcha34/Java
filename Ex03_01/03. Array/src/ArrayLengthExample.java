cbe113cb66f236164f2e86558e78389cbdd8a7ab
barn485 <barn485@gmail.com>
2020-11-05 12:24:28 +0900

    Array

--- Ex03_01/03. Array/src/ArrayLengthExample.java
+++ Ex03_01/03. Array/src/ArrayLengthExample.java
@@ -13,6 +13,8 @@ public class ArrayLengthExample {
 		System.out.println("총합 : " + sum);
 		
 		double avg = (double) sum / scores.length;
+		// 정수 나누기 정수는 int
+		// 따라서 (double)을 제외하면 86.0 값이 나온다.
 		System.out.println("평균 : " + avg);
 
 	}
